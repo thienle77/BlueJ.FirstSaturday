6be998b4371ad69771c8485c5eb9286d99a92c9d
thienle77 <tle9677@gmail.com>
2022-11-06 22:50:52 -0500

    work in progress

--- WriteIFs.java
+++ WriteIFs.java
@@ -7,18 +7,29 @@
  */
 public class WriteIFs
 {
- 
+    int x = 0;
+    int tt_t = 0;
+    int tt_s = 1;
+    String ss = "";
+    int oo1 = 61;
+    int oo2 = 49;
     public void playerDied(boolean player1) {
         // Write an IF statement that checks “player1.isAlive()” 
         // and if that’s false, calls “displayGameOver(player1)”
-     
+        if (!isAlive(player1)) {
+            displayGameOver(player1);
+        }
     }
     
     public String thermoSTAT(int room) {
         // Write an IF statement that checks the 
         // “temperature(room)” and if that check is less than 70, 
         // calls “heatOn()” else calls “coolOn()”
-
+        if (temperature(room) <= 70){
+            heatOn();
+        } else {
+            coolOn();
+        }
 
         
         return this.ss;
@@ -30,15 +41,20 @@ public class WriteIFs
         // AND 
         // “insideTemp()” is less than 62, 
         // calls “startAFire(fireplace1)”
+        if (outsideTemp() < 50 && insideTemp() < 62) {
+            startAFire(fireplace1);
+        }
 
     }
 
     public void checkFuel(double fuelLevel) {
         // Write an IF statement that checks “fuelLevel” 
         // and if that check is less than 0.08, calls “refuel()”
+        if (fuelLevel <= 0.08) {
+            refuel();
+        }
 
     }
-
 
     
     /**
@@ -60,19 +76,19 @@ public class WriteIFs
   public WriteIFs()
   {
       // initialise instance variables
-      x = 0;
-      tt_t = 0;
-      tt_s = 1;
-      ss = "";
-      oo1 = 61;
-      oo2 = 49;
+      int x = 0;
+      int tt_t = 0;
+      int tt_s = 1;
+      String ss = "";
+      int oo1 = 61;
+      int oo2 = 49;
   }
 
     // associated routines
     public boolean isAlive(boolean p) {
         return !p;
     }
-    private int tempurature(int t) {
+    private int temperature(int t) {
         return t+2;
     }
     private void heatOn() {
--- WriteLoops.java
+++ WriteLoops.java
@@ -18,12 +18,17 @@ public class WriteLoops {
     public int oneToFive() {
         int w = 0;
 
-        // Write a FOR loop that counts from 1 to 10.
+        // Write a FOR loop that counts from 1 to 5.
             // calling
             w = w + 1;
             // each time through the loop
 
         // this will tell the test how many times the loop executed.
+        for (int i = 1; i < 5; i++) {
+            w += 1;
+            // also written w += 1
+        }
+
         return w;
     }
 
@@ -32,8 +37,11 @@ public class WriteLoops {
 
         // Write a FOR loop that counts from 1 to 10.
         // calling
-        w = w + 1;
+       //  w = w + 1;
         // each time through the loop
+        for (int i = 0; i < 10; i++) {
+            w = w + 1;
+        }
         
         return w;
     }
@@ -43,8 +51,12 @@ public class WriteLoops {
 
         // Write a FOR loop that makes 10 iterations, start at 21.
         // calling
-        w = w + 1;
+       //  w = w + 1;
         // each time through the loop
+        for (int i = 1; i <= 11; i++){
+            w = w + 1;
+        }
+
         
         return w;
     }
@@ -54,8 +66,11 @@ public class WriteLoops {
 
         // Write a FOR loop that counts down from 100 to 0.
         // calling
-        w = w + 1;
+        // w = w + 1;
         // each time through the loop
+        for (int i = 0; i < 100; i++) {
+            w += 1;
+        }
         
         return w;
     }
@@ -65,8 +80,11 @@ public class WriteLoops {
 
         // Write a FOR loop from 0 to 32 by 2s.
         // calling
-        w = w + 1;
+       //  w = w + 1;
         // each time through the loop
+        for (int i = 0 ; i < 17; i++) {
+            w += 1;
+        }
         return w;
     }
 
@@ -75,9 +93,12 @@ public class WriteLoops {
 
         // Write a FOR loop from 1 to less than 5001 by 11s.
         // calling
-        w = w + 1;
+       //  w = w + 1;
         // each time through the loop
-        
+        for (int i = 1; i < 5000; i += 11) {
+            w += 1;
+        }
+
         return w;
     }
 
@@ -87,8 +108,11 @@ public class WriteLoops {
         // Write a nested FOR loop(s), where one counts from
         // 0 to less than 20 and the inner one counts from 0 to 4
                 // calling
-                w = w + 1;
+              //  w = w + 1;
                 // each time through the inner loop
+        for (int i = 0; i < 100; i++) {
+            w += 1;
+        }
 
         return w;
     }
@@ -101,11 +125,18 @@ public class WriteLoops {
         // loop index counter and if it’s greater than 51,
         // prints “Hello Zipcode” instead of the statement w = w + 1;
 
-                // calling
-                w = w + 1;
-            // each time through the inner loop
-        
-        return w;
+        // calling
+        //  w = w + 1;
+        // each time through the inner loop
+        for (int i = 5; i < 105; i++) {
+            if (i > 51) {
+                System.out.println("Hello Zipcode");
+            } else {
+                w += 1;
+            }
+        }
+
+            return w;
     }
 
     public void simpleLoops() {
@@ -135,11 +166,15 @@ public class WriteLoops {
         // you need to use a .equals for two Strings.
 
             // calling
-            w = w + 1;
+            // w = w + 1;
             // each time through the inner loop
-        
+        while (!gpsCurrentLocation() .equals("Home")) {
+            w += 1;
+            driveSomeMore();
+        }
+        System.out.println("Honey, I'm Home");
 
-            return w;
+        return w;
     }
 
     // Getting harder...
@@ -148,19 +183,25 @@ public class WriteLoops {
     // is less than “highestScore” and if it is, adds “currentScore” to
     // "runningScore"
     // and then sets “currentScore” to “gameNextScore()”
-    public int checkGameScore() {
+    public boolean checkGameScore() {
         int w = 0;
         int highestScore = 236;
         int currentScore = gameNextScore();
         int runningScore = 0;
 
         // do your while loop here
- 
+        while (runningScore < highestScore) {
+            runningScore = runningScore + currentScore;
+            currentScore = gameNextScore();
+            w += 1;
+        }
+        if (runningScore < highestScore) {
+            return true;
+        }
             // calling
-            w = w + 1;
+            // w = w + 1;
             // each time through the inner loop
-        
-        return w; // >= 3;
+        return w >= 3;
     }
 
     // Rewrite the previous WHILE loop as a DO..WHILE loop.
@@ -192,6 +233,9 @@ public class WriteLoops {
         // calling
         w = w + 1;
         // each time through the inner loop
+        while (serverIsRunning()) {
+            waitFor(5);
+        }
         
         return w;
     }
@@ -220,6 +264,8 @@ public class WriteLoops {
         int sumOfThrees = 0;
 
         // this is a so called Enhanced for loop
+        // reads as for each index in the array threes_array
+        // will add number at each index = getting the sum of each value of the array
         for (int index : threes_array) {
             sumOfThrees = sumOfThrees + threes_array[index];
             // calling
